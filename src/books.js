0196542090dc257066813e286c8207a9da1b5163
Fawaz <you@example.com>
2023-11-20 18:12:57 +0530

    Initial commit

---
+++ src/books.js
@@ -0,0 +1,23 @@
+import img1 from "./images/book-1.jpg";
+import img2 from "./images/book-2.jpg";
+import img3 from "./images/book-3.jpg";
+export const books = [
+  {
+    id: 1,
+    author: "Ankit Bhati",
+    title: "SSC GD",
+    img: img1,
+  },
+  {
+    id: 2,
+    author: "James Clear",
+    title: "Atomic Habits",
+    img: img2,
+  },
+  {
+    id: 3,
+    author: "Joseph Nguyen",
+    title: "Don't Believe Everything You Think ",
+    img: img3,
+  },
+];
